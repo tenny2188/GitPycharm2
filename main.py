6cdbbb006b6b0b40f5fa54436acf66523e70779a
TaeHun Kwon <tenny210@gmail.com>
2022-04-01 19:21:01 +0900

    Update on Local Master Branch

--- main.py
+++ main.py
@@ -15,5 +15,8 @@ print("Hello World 4 ")
 # adding new item in master branch
 print("Hello World 5 ")
 
+# Updates on local master branch
+print("Hello World Updates on Local Master Branch ")
+
 # Updates on mainbranch of Github
 print("Hello World Updates on Github")
